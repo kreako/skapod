7dd289b31697b3bf09f3f32114faec244a478df7
kreako <olivier@kreako.fr>
2021-11-14 13:16:28 +0100

    main script and Home page

---
+++ frontend/src/pages/Home.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div className="grid grid-cols-3 grid-rows-3 place-items-center h-screen">
+      <div className="col-start-2 row-start-2 ">
+        <div className="text-5xl"> 🎯</div>
+      </div>
+    </div>
+  )
+}
