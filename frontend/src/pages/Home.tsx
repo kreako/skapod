fdf6a8c3cc1365a5815cdc24539a49a9e0548d6a
kreako <olivier@kreako.fr>
2021-11-15 20:56:43 +0100

    Add a fly me to the moon component to play a Frank song

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,8 +1,10 @@
+import FlyMeToTheMoon from "../components/FlyMeToTheMoon"
+
 export default function Home() {
   return (
-    <div className="grid grid-cols-3 grid-rows-3 place-items-center h-screen">
-      <div className="col-start-2 row-start-2 ">
-        <div className="text-5xl"> 🎯</div>
+    <div className="flex items-center px-8 h-screen">
+      <div className="flex-grow">
+        <FlyMeToTheMoon />
       </div>
     </div>
   )
