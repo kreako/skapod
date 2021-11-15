fc4db092aee4bb642b7dbfb1269ce00c962784a2
kreako <olivier@kreako.fr>
2021-11-15 20:46:09 +0100

    Add query client provider for react query

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,18 @@
 import React from "react"
+import { QueryClient, QueryClientProvider } from "react-query"
 import { HashRouter, Routes, Route } from "react-router-dom"
 import Home from "./pages/Home"
 
+const queryClient = new QueryClient()
+
 export default function App() {
   return (
-    <HashRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-      </Routes>
-    </HashRouter>
+    <QueryClientProvider client={queryClient}>
+      <HashRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </HashRouter>
+    </QueryClientProvider>
   )
 }
