7baf02409a4996c0df9d066d4ef3b1af2a92b36c
kreako <olivier@kreako.fr>
2021-11-21 14:39:24 +0100

    Better scale handling with round units

--- frontend/src/pages/Layout.tsx
+++ frontend/src/pages/Layout.tsx
@@ -38,15 +38,50 @@ function TimeScale() {
   const pxPerSeconds = useStore((state) => state.pxPerSeconds)
   const windowSize = useWindowSize()
 
-  // I want 1 marker very ~100 pixels
-  const markerIdealSpacing = 100
-  const secondsPerMarker = roundToClosestScale(
-    markerIdealSpacing / pxPerSeconds
-  )
+  // Stupid default
+  let secondsPerMarker = 1
+  // the biggest label I will have is something like 09:59:59
+  // it is around 64px wide so the spacing between 2 markers should not
+  // be less than 64 + 20 = 84px
+  const markerMinimumSpacing = 84
+  if (pxPerSeconds > markerMinimumSpacing) {
+    // subSeconds
+    const subSecondsScale = [1, 0.5, 0.2, 0.1]
+    let idx = 0
+    while (subSecondsScale[idx] * pxPerSeconds > markerMinimumSpacing) {
+      idx += 1
+    }
+    secondsPerMarker = subSecondsScale[idx - 1]
+  } else if (60 * pxPerSeconds > markerMinimumSpacing) {
+    // seconds to minutes
+    const secondsScale = [60, 30, 20, 10, 5, 2, 1]
+    let idx = 0
+    while (secondsScale[idx] * pxPerSeconds > markerMinimumSpacing) {
+      idx += 1
+    }
+    secondsPerMarker = secondsScale[idx - 1]
+  } else {
+    // minutes to hours
+    const minutesScale = [
+      5 * 60 * 60,
+      2 * 60 * 60,
+      1 * 60 * 60,
+      30 * 60,
+      20 * 60,
+      10 * 60,
+      5 * 60,
+      2 * 60,
+      1 * 60,
+    ]
+    let idx = 0
+    while (minutesScale[idx] * pxPerSeconds > markerMinimumSpacing) {
+      idx += 1
+    }
+    secondsPerMarker = minutesScale[idx - 1]
+  }
   let displaySubSecond = secondsPerMarker < 1
   const markerWidth = secondsPerMarker * pxPerSeconds
   const markerNb = Math.floor(windowSize.width / markerWidth)
-  console.log(secondsPerMarker, markerWidth, markerNb)
   const markers = []
   for (let idx = 0; idx < markerNb; idx++) {
     markers.push({
@@ -71,6 +106,7 @@ function TimeScale() {
           </div>
         ))}
       </div>
+      {/* Ticks */}
       <div>
         {markers.map((m) => (
           <div
@@ -132,7 +168,12 @@ export default function Layout() {
   return (
     <div className="flex h-screen">
       <div className="flex-grow flex flex-col">
-        <div className="bg-blue-200 h-16"> Toolbar : {pxPerSeconds}</div>
+        <div className="bg-blue-200 h-16">
+          Toolbar : {pxPerSeconds}
+          <div>
+            <span>09:59:59</span>
+          </div>
+        </div>
         <div className="flex w-screen flex-grow">
           <div className="bg-orange-200 w-16">Header</div>
           <div className="flex-grow relative">
