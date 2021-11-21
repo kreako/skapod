e0bc88456fcd0e7d9e53e38d92f4c428ba95406e
kreako <olivier@kreako.fr>
2021-11-21 21:13:51 +0100

    Factor max/min of zoom for reuse

--- frontend/src/store.tsx
+++ frontend/src/store.tsx
@@ -9,18 +9,21 @@ type BearState = {
 const SCALE_IN = Math.exp(0.2)
 const SCALE_OUT = Math.exp(-0.2)
 
+export const MAX_ZOOM_IN = 2000
+export const MIN_ZOOM_OUT = 0.01
+
 export const useStore = create<BearState>((set) => ({
   pxPerSeconds: 5,
   horizontalZoomIn: () =>
     set((state) => {
-      if (state.pxPerSeconds < 2000) {
+      if (state.pxPerSeconds < MAX_ZOOM_IN) {
         // limit zoom in at 2000 px per seconds seems reasonable 1 full seconds on a large screen
         return { pxPerSeconds: state.pxPerSeconds * SCALE_IN }
       }
     }),
   horizontalZoomOut: () =>
     set((state) => {
-      if (state.pxPerSeconds > 0.01) {
+      if (state.pxPerSeconds > MIN_ZOOM_OUT) {
         // limit zoom out at 0.01 px per seconds
         // a 2000px wide screen displays around 55 hours
         // seems scrolling is reasonable after 55 hours of content
