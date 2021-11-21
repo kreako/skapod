657c7d4ab5f8d0a3aa5647f286d30131df6e7062
kreako <olivier@kreako.fr>
2021-11-21 13:03:27 +0100

    Limit zoom in and out to "reasonable view"

--- frontend/src/store.tsx
+++ frontend/src/store.tsx
@@ -12,7 +12,19 @@ const SCALE_OUT = Math.exp(-0.2)
 export const useStore = create<BearState>((set) => ({
   pxPerSeconds: 5,
   horizontalZoomIn: () =>
-    set((state) => ({ pxPerSeconds: state.pxPerSeconds * SCALE_IN })),
+    set((state) => {
+      if (state.pxPerSeconds < 2000) {
+        // limit zoom in at 2000 px per seconds seems reasonable 1 full seconds on a large screen
+        return { pxPerSeconds: state.pxPerSeconds * SCALE_IN }
+      }
+    }),
   horizontalZoomOut: () =>
-    set((state) => ({ pxPerSeconds: state.pxPerSeconds * SCALE_OUT })),
+    set((state) => {
+      if (state.pxPerSeconds > 0.01) {
+        // limit zoom out at 0.01 px per seconds
+        // a 2000px wide screen displays around 55 hours
+        // seems scrolling is reasonable after 55 hours of content
+        return { pxPerSeconds: state.pxPerSeconds * SCALE_OUT }
+      }
+    }),
 }))
