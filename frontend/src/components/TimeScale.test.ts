fe83feece27f190f6bea4a0b6549cdafcf84baa3
kreako <olivier@kreako.fr>
2021-11-22 12:39:57 +0100

    Add another test, making sure scale markers are not too far from each others

--- frontend/src/components/TimeScale.test.ts
+++ frontend/src/components/TimeScale.test.ts
@@ -1,7 +1,7 @@
 import { MAX_ZOOM_IN, MIN_ZOOM_OUT, ZOOM_SCALE_IN } from "../store"
 import { MARKER_MINIMUM_SPACING, secondsPerMarker } from "./TimeScale"
 
-test("secondsPerMarker returns seconds leading to markers spacing > MARKER_MINIMUM_SPACING", () => {
+test("secondsPerMarker - markers spacing not too close", () => {
   // so secondsPerMarker * pxPerSeconds > MARKER_MINIMUM_SPACING for every value of pxPerSeconds
   for (
     let pxPerSeconds = MIN_ZOOM_OUT;
@@ -10,6 +10,19 @@ test("secondsPerMarker returns seconds leading to markers spacing > MARKER_MINIM
   ) {
     expect(secondsPerMarker(pxPerSeconds)).toBeGreaterThanOrEqual(
       MARKER_MINIMUM_SPACING / pxPerSeconds
+    )
+  }
+})
+
+test("secondsPerMarker - markers spacing not too far", () => {
+  // so secondsPerMarker * pxPerSeconds < 2.5 * MARKER_MINIMUM_SPACING for every value of pxPerSeconds
+  for (
+    let pxPerSeconds = MIN_ZOOM_OUT;
+    pxPerSeconds < MAX_ZOOM_IN;
+    pxPerSeconds = pxPerSeconds * ZOOM_SCALE_IN
+  ) {
+    expect(secondsPerMarker(pxPerSeconds)).toBeLessThanOrEqual(
+      (2.5 * MARKER_MINIMUM_SPACING) / pxPerSeconds
     )
   }
 })
