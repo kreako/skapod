d448bbc987f55f846ec6f692f43f036cb5713ce6
kreako <olivier@kreako.fr>
2021-11-24 19:52:30 +0100

    Enable msw for dev mode

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,6 +3,13 @@ import ReactDOM from "react-dom"
 import "./index.css"
 import App from "./App"
 
+if (process.env.NODE_ENV === "development") {
+  ;(async () => {
+    const mod = await import("./mocks/browser")
+    mod.worker.start()
+  })()
+}
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
---
+++ frontend/src/mocks/handlers.ts
@@ -0,0 +1,53 @@
+import { rest } from "msw"
+
+export const handlers = [
+  rest.get("/project", (req, res, ctx) => {
+    return res(
+      ctx.status(200),
+      ctx.json({
+        tracksNb: 3,
+        clipsNb: 5,
+        maxTime: 32 * 60 + 27,
+        sources: [
+          { id: "0", kind: "record", length: 3 * 60 + 12, datas: {} },
+          { id: "1", kind: "sample", length: 7 * 60 + 23, datas: {} },
+          { id: "2", kind: "record", length: 23 * 60 + 13, datas: {} },
+        ],
+        clips: [
+          { id: "0", source: "0", start: 12, length: 29 },
+          { id: "1", source: "0", start: 22, length: 2 * 60 + 3 },
+          { id: "2", source: "1", start: 2 * 60 + 23, length: 29 },
+          { id: "3", source: "1", start: 12, length: 5 },
+          { id: "4", source: "2", start: 2, length: 23 * 60 + 11 },
+        ],
+        tracks: [
+          {
+            id: "0",
+            title: "Voice 1",
+            clips: [
+              { clip: "0", start: 0 },
+              { clip: "0", start: 19 },
+              { clip: "1", start: 42 },
+              { clip: "0", start: 42 + 2 * 60 + 3 - 5 + 29 - 1 },
+            ],
+          },
+          {
+            id: "1",
+            title: "Music",
+            clips: [
+              { clip: "2", start: 42 + (2 * 60 + 3) - 5 },
+              { clip: "3", start: 42 + 2 * 60 + 3 - 5 + 29 - 1 + 29 - 1 },
+            ],
+          },
+          {
+            id: "0",
+            title: "Voice 2",
+            clips: [
+              { clip: "4", start: 42 + 2 * 60 + 3 - 5 + 29 - 1 + 29 - 1 + 5 },
+            ],
+          },
+        ],
+      })
+    )
+  }),
+]
