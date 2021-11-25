03bc695a05bbc49a6bce81d825e1afca9d72d184
kreako <olivier@kreako.fr>
2021-11-25 14:00:44 +0100

    staleTime infinity for the project query

--- frontend/src/pages/Layout.tsx
+++ frontend/src/pages/Layout.tsx
@@ -71,7 +71,7 @@ export default function Layout() {
     }
   })
 
-  const project = useQuery("project", fetchProject)
+  const project = useQuery("project", fetchProject, { staleTime: Infinity })
 
   return (
     <div className="flex h-screen">
