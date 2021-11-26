08bdf57c16c30343481af461473938c80eed4916
kreako <olivier@kreako.fr>
2021-11-26 16:38:14 +0100

    use a mutation to set display mode on track

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,15 +1,81 @@
-import { trackForMutations } from "@reduxjs/toolkit/dist/immutableStateInvariantMiddleware"
-import { useQuery } from "react-query"
-import { fetchProject, ProjectType } from "../api"
+import { useMutation, useQuery, useQueryClient } from "react-query"
+import {
+  fetchProject,
+  MutDisplayType,
+  ProjectType,
+  setTrackDiplay,
+  TrackDisplayType,
+} from "../api"
 import { HEADER_WIDTH_CLASSNAME } from "../utils/ui"
 import TrackHeader from "./TrackHeader"
+import produce from "immer"
 
 export default function Header() {
+  const queryClient = useQueryClient()
+
   const project = useQuery("project", fetchProject, { staleTime: Infinity })
+
+  const displayMutation = useMutation(setTrackDiplay, {
+    onMutate: async ({ display, trackId }: MutDisplayType) => {
+      await queryClient.cancelQueries("project")
+      const previousProject = queryClient.getQueryData<ProjectType>("project")
+      if (previousProject) {
+        const idx = previousProject.tracks.findIndex((t) => t.id === trackId)
+        if (idx !== -1) {
+          queryClient.setQueryData<ProjectType>(
+            "project",
+            produce(previousProject, (draft) => {
+              draft.tracks[idx].display = display
+            })
+          )
+        }
+      }
+      return { previousProject }
+    },
+    onError: (err, variables, context) => {
+      if (context?.previousProject) {
+        queryClient.setQueryData<ProjectType>(
+          "project",
+          context.previousProject
+        )
+      }
+    },
+    // Always refetch after error or success:
+    onSettled: () => {
+      queryClient.invalidateQueries("project")
+    },
+  })
+
+  const getTrackHandlers = (trackId) => ({
+    clickDisplay: (display: TrackDisplayType) => {
+      displayMutation.mutate({ trackId, display })
+    },
+    clickMenu: () => {
+      console.log("clickMenu")
+    },
+    clickMute: () => {
+      console.log("clickMute")
+    },
+    clickSolo: () => {
+      console.log("clickSolo")
+    },
+    clickVolume: () => {
+      console.log("clickVolume")
+    },
+    clickPanLR: () => {
+      console.log("clickPanLR")
+    },
+    clickTitle: () => {
+      console.log("clickTitle")
+    },
+  })
+
   return (
     <div className={`bg-sky-300 ${HEADER_WIDTH_CLASSNAME} pt-12`}>
       {project.isSuccess &&
-        project.data.tracks.map((t) => <TrackHeader key={t.id} track={t} />)}
+        project.data.tracks.map((t) => (
+          <TrackHeader key={t.id} track={t} handlers={getTrackHandlers(t.id)} />
+        ))}
     </div>
   )
 }
