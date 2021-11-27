313c3ca9c2b8e24f88753449ade468f6636ee45d
kreako <olivier@kreako.fr>
2021-11-27 18:58:22 +0100

    Add colors to clip stories

--- frontend/src/components/Clip.stories.tsx
+++ frontend/src/components/Clip.stories.tsx
@@ -16,15 +16,39 @@ export default {
 
 const Template: ComponentStory<typeof Clip> = (args) => <Clip {...args} />
 
-export const Default = Template.bind({})
-Default.args = {
+export const Blue = Template.bind({})
+Blue.args = {
   length: 300,
   display: "full",
   color: "blue",
 }
 
-export const Mini = Template.bind({})
-Mini.args = {
-  ...Default.args,
+export const Green = Template.bind({})
+Green.args = {
+  ...Blue.args,
+  color: "green",
+}
+
+export const Gray = Template.bind({})
+Gray.args = {
+  ...Blue.args,
+  color: "gray",
+}
+
+export const Yellow = Template.bind({})
+Yellow.args = {
+  ...Blue.args,
+  color: "yellow",
+}
+
+export const Orange = Template.bind({})
+Orange.args = {
+  ...Blue.args,
+  color: "orange",
+}
+
+export const MiniBlue = Template.bind({})
+MiniBlue.args = {
+  ...Blue.args,
   display: "mini",
 }
