bc6ec57c5934ddfddf8c1d002fc278bd132d0e97
kreako <olivier@kreako.fr>
2021-11-28 20:12:21 +0100

    Display clips

--- frontend/src/components/Clip.tsx
+++ frontend/src/components/Clip.tsx
@@ -5,13 +5,22 @@ import { ColorType, TrackDisplayType } from "../api"
 import { resizeWithPixelRatio } from "../utils/canvas"
 import { colorToBgClassName, colorToValue } from "../utils/colors"
 import {
+  CLIP_HEADER_HEIGHT_REM,
+  remToPx,
   TRACK_HEIGHT_FULL_CLASSNAME,
+  TRACK_HEIGHT_FULL_REM,
   TRACK_HEIGHT_MINI_CLASSNAME,
+  TRACK_HEIGHT_MINI_REM,
 } from "../utils/ui"
 
-export function ClipCanvas({ color }: ClipProps) {
+export function ClipCanvas({ display, color, left, top, length }: ClipProps) {
   let canvasRef = useRef<HTMLCanvasElement>()
   const c = colorToValue(color)
+  const height =
+    display === "full"
+      ? remToPx(TRACK_HEIGHT_FULL_REM - CLIP_HEADER_HEIGHT_REM)
+      : remToPx(TRACK_HEIGHT_MINI_REM)
+  const width = length
 
   useEffect(() => {
     if (!canvasRef.current) {
@@ -23,7 +32,7 @@ export function ClipCanvas({ color }: ClipProps) {
     const ctx = canvas.getContext("2d")
 
     // Set width/height taking pixel ratio in account
-    resizeWithPixelRatio(canvas, ctx)
+    resizeWithPixelRatio(canvas, ctx, width, height)
 
     // Now for the drawing
     ctx.clearRect(0, 0, canvas.width, canvas.height)
@@ -37,16 +46,21 @@ export function ClipCanvas({ color }: ClipProps) {
     )
 
     console.log(canvas.width, canvas.height)
-  }, [canvasRef])
+  }, [canvasRef, color, left, top, length])
   return <canvas ref={canvasRef} className="w-full h-full" />
 }
 
 export function ClipMini(props: ClipProps) {
   const bg = colorToBgClassName(props.color)
+  const style = {
+    width: `${props.length}px`,
+    top: `${props.top}px`,
+    left: `${props.left}px`,
+  }
   return (
     <div
-      className={`${bg} absolute top-0 left-0 ${TRACK_HEIGHT_MINI_CLASSNAME} border border-sky-800 rounded-md z-10`}
-      style={{ width: `${props.length}px` }}
+      className={`${bg} absolute ${TRACK_HEIGHT_MINI_CLASSNAME} border border-sky-800 rounded-md z-10`}
+      style={style}
     >
       <ClipCanvas {...props} />
     </div>
@@ -55,14 +69,19 @@ export function ClipMini(props: ClipProps) {
 
 export function ClipFull(props: ClipProps) {
   const bg = colorToBgClassName(props.color)
+  const style = {
+    width: `${props.length}px`,
+    top: `${props.top}px`,
+    left: `${props.left}px`,
+  }
   return (
     <div
-      className={`${bg} absolute top-0 left-0 ${TRACK_HEIGHT_FULL_CLASSNAME} border border-sky-800 rounded-md z-10`}
-      style={{ width: `${props.length}px` }}
+      className={`${bg} absolute ${TRACK_HEIGHT_FULL_CLASSNAME} border border-sky-800 rounded-md z-10`}
+      style={style}
     >
       <div className="absolute top-0 inset-x-0 flex flex-col">
         <div className="flex items-center px-1 mt-1 border-b border-sky-800">
-          <div>Clip name</div>
+          <div>{props.id}</div>
           <div className="flex-grow"></div>
           <div className="flex-grow-0">
             <DotsVerticalIcon className="h-4 w-4" />
@@ -77,7 +96,10 @@ export function ClipFull(props: ClipProps) {
 }
 
 type ClipProps = {
+  id: string
   display: TrackDisplayType
+  top: number
+  left: number
   length: number
   color: ColorType
 }
--- frontend/src/components/Clips.tsx
+++ frontend/src/components/Clips.tsx
@@ -1,22 +1,98 @@
 import React from "react"
 import { useElementSize } from "usehooks-ts"
 import { ClipType, ProjectType, TrackType } from "../api"
+import { useStore } from "../store"
+import { fromArrayToIdObjects } from "../utils/objects"
 import {
+  remToPx,
+  TIMESCALE_HEIGHT_REM,
   TRACK_HEIGHT_FULL_CLASSNAME,
+  TRACK_HEIGHT_FULL_REM,
   TRACK_HEIGHT_MINI_CLASSNAME,
+  TRACK_HEIGHT_MINI_REM,
+  TRACK_SEPARATOR_HEIGHT_CLASSNAME,
+  TRACK_SEPARATOR_HEIGHT_REM,
 } from "../utils/ui"
+import { isClipInView, timeEndView } from "../utils/view"
+import Clip from "./Clip"
 
-type TrackProps = {
-  track: TrackType
-  clips: ClipType[]
+type ClipsIndexType = {
+  [id: string]: ClipType
 }
 
-export function Track({ track, clips }: TrackProps) {
+type TrackProps = {
+  index: number
+  track: TrackType
+  clips: ClipsIndexType
+  start: number
+  end: number
+  pxPerSeconds: number
+}
+
+export function Track({
+  index,
+  track,
+  clips,
+  start,
+  end,
+  pxPerSeconds,
+}: TrackProps) {
   let heightTrack = TRACK_HEIGHT_FULL_CLASSNAME
+  let heightTrackRem = TRACK_HEIGHT_FULL_REM
   if (track.display === "mini") {
     heightTrack = TRACK_HEIGHT_MINI_CLASSNAME
+    heightTrackRem = TRACK_HEIGHT_MINI_REM
   }
-  return <div className={`${heightTrack}`}></div>
+  const clipsInView = track.content
+    .filter((c) => {
+      const clipId = c.clip
+      const clip = clips[clipId]
+      if (track.id === "0" && clip.id === "0") {
+        console.log(
+          start,
+          end,
+          c.start,
+          clip.length,
+          isClipInView(start, end, c.start, clip.length)
+        )
+      }
+      return isClipInView(start, end, c.start, clip.length)
+    })
+    .map((c) => {
+      const clipId = c.clip
+      const clipStartInTrack = c.start
+      const clip = clips[clipId]
+      return {
+        id: c.id,
+        clipStartInTrack: clipStartInTrack,
+        topPx: remToPx(
+          index * (heightTrackRem + TRACK_SEPARATOR_HEIGHT_REM) +
+            TIMESCALE_HEIGHT_REM
+        ),
+        leftPx: (clipStartInTrack - start) * pxPerSeconds,
+        length: clip.length,
+        lengthPx: clip.length * pxPerSeconds,
+        color: clip.color,
+      }
+    })
+  if (track.id === "0") {
+    console.log(start, end, clipsInView)
+  }
+  return (
+    <div className={`${heightTrack}`}>
+      {clipsInView.map((clip) => (
+        <Clip
+          key={clip.id}
+          id={clip.id}
+          display={track.display}
+          top={clip.topPx}
+          left={clip.leftPx}
+          length={clip.lengthPx}
+          color={clip.color}
+        />
+      ))}
+    </div>
+  )
 }
 
 type ClipsProps = {
@@ -25,14 +101,29 @@ type ClipsProps = {
 
 export default function Clips({ project }: ClipsProps) {
   const [rootRef, { width, height }] = useElementSize()
+  const { pxPerSeconds, start } = useStore((state) => ({
+    pxPerSeconds: state.pxPerSeconds,
+    start: state.start,
+  }))
+  const end = timeEndView(start, pxPerSeconds, width)
+  const clipsIndex = fromArrayToIdObjects(project.clips)
   return (
     <div ref={rootRef} className="absolute inset-0">
       {/* in sync with pt-12 in Header component */}
       <div className="mt-12">
-        {project.tracks.map((t) => (
+        {project.tracks.map((t, index) => (
           <React.Fragment key={t.id}>
-            <Track track={t} clips={project.clips} />
-            <div className="my-2 h-1 bg-sky-400"></div>
+            <Track
+              index={index}
+              track={t}
+              clips={clipsIndex}
+              start={start}
+              end={end}
+              pxPerSeconds={pxPerSeconds}
+            />
+            <div
+              className={`${TRACK_SEPARATOR_HEIGHT_CLASSNAME} bg-sky-400`}
+            ></div>
           </React.Fragment>
         ))}
         {width} * {height}
