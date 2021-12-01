e8d57d3c87607dbe7ba5ebab0629041c8e507a99
kreako <olivier@kreako.fr>
2021-12-01 19:18:46 +0100

    Reorganize types without tracks

--- frontend/src/api.ts
+++ frontend/src/api.ts
@@ -1,58 +1,12 @@
 import axios from "axios"
+import { ProjectType } from "./types"
 
-type DataType = {}
-
-export type SourceKindType = "record" | "sample"
-
-type SourceType = {
-  id: string
-  kind: SourceKindType
-  length: number
-  data: DataType
-}
-
-export type ColorType = "blue" | "green" | "gray" | "yellow" | "orange"
-
-export type ClipType = {
-  id: string
-  source: string
-  start: number
-  length: number
-  color: ColorType
-}
-
-type TrackContentType = {
-  id: string
-  clip: string
-  start: number
-}
-
-export type TrackDisplayType = "full" | "mini"
-
-export type TrackType = {
-  id: string
-  title: string
-  volume: number
-  panLR: number
-  display: TrackDisplayType
-  content: TrackContentType[]
-}
-
-export type ProjectType = {
-  tracksNb: number
-  clipsNb: number
-  maxTime: number
-  sources: SourceType[]
-  clips: ClipType[]
-  tracks: TrackType[]
-}
-
-export const fetchProject = async (): Promise<ProjectType> => {
-  const res = await axios.get("/project")
+export const fetchProject = async (id: string): Promise<ProjectType> => {
+  const res = await axios.get(`/project/${id}`)
   return res.data
 }
 
-export type MutDisplayType = {
+/*export type MutDisplayType = {
   display: TrackDisplayType
   trackId: string
 }
@@ -62,4 +16,4 @@ export const setTrackDiplay = async ({
   display,
 }: MutDisplayType): Promise<void> => {
   await axios.patch(`/project/track/${trackId}/display`, { display })
-}
+}*/
---
+++ frontend/src/types.ts
@@ -0,0 +1,70 @@
+export type ProjectType = {
+  id: string
+  sources: SourceType[]
+  clips: ClipType[]
+  groups: GroupType[]
+}
+
+// Source : a sound
+type SourceType = {
+  id: string
+  name: string
+  kind: SourceKindType
+  length: number
+  url: string
+  // pre-computed peaks profile
+  peaks: {}
+}
+
+export enum SourceKindType {
+  Record = "record",
+  Sample = "sample",
+}
+
+// Clip : a piece of a sound
+export enum ColorType {
+  Blue = "blue",
+  Green = "green",
+  Gray = "gray",
+  Yellow = "yellow",
+  Orange = "orange",
+}
+
+export type ClipType = {
+  id: string
+  name: string
+  source: string
+  start: number
+  length: number
+  color: ColorType
+}
+
+// Group : group contains clips and (sub-)groups
+export type GroupType = {
+  start: number
+  content: GroupContentType[]
+  display: DisplayType
+}
+
+export enum DisplayType {
+  Full = "full",
+  Mini = "mini",
+}
+
+export enum GroupContentKindType {
+  Group = "group",
+  Clip = "clip",
+}
+
+// Instance of a clip in a group
+export type ClipInGroupType = {
+  id: string
+  clip: string
+  start: number
+  display: DisplayType
+}
+
+export type GroupContentType = {
+  kind: GroupContentKindType
+  data: GroupType | ClipInGroupType
+}
