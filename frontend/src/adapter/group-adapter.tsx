dbf64c969ac6cc921bb81c26fd6a7d093eb09b67
kreako <olivier@kreako.fr>
2021-12-09 15:40:31 +0100

    Add group content props (clip and group) in group-adapter

--- frontend/src/adapter/group-adapter.tsx
+++ frontend/src/adapter/group-adapter.tsx
@@ -1,6 +1,6 @@
 import { GroupInstanceType } from "../api/group-instance"
-import { GroupProps } from "../components/Group"
-import { CLIP_BOTTOM_MARGIN_PX } from "./ui"
+import { ColorType, GroupContentKindType, GroupDisplayType } from "../api/types"
+import { GroupChildProps, GroupProps } from "../components/Group"
 
 export type GroupAdapterParams = {
   groupInstance: GroupInstanceType
@@ -9,6 +9,93 @@ export type GroupAdapterParams = {
   viewStart: number
   pxPerSeconds: number
   clipHeight: number
+}
+
+function produceChildrenProps(
+  rootInstance: GroupInstanceType,
+  display: GroupDisplayType,
+  color: ColorType,
+  pxPerSeconds: number,
+  clipHeight: number
+): GroupChildProps[] {
+  const children = []
+  const root = rootInstance.group()
+
+  for (const content of root.content.content) {
+    if (content.kind === GroupContentKindType.Clip) {
+      const clipInstance = content.asClipInstance()
+      const clip = clipInstance.clip()
+      let top = null
+      if (
+        display === GroupDisplayType.Collapsed ||
+        rootInstance.display === GroupDisplayType.Collapsed
+      ) {
+        top = 0
+      } else {
+        top = clipHeight * clipInstance.row
+      }
+      const width = Math.floor(clip.length * pxPerSeconds)
+      const left = clipInstance.start * pxPerSeconds
+
+      children.push({
+        id: content.id(),
+        kind: GroupContentKindType.Clip,
+        props: {
+          id: clipInstance.id,
+          name: clip.name,
+          top,
+          left,
+          width,
+          height: clipHeight,
+          color: color,
+          muted: clipInstance.muted,
+          displayHeader: false, // always off inside a group
+        },
+      })
+    } else {
+      const groupInstance = content.asGroupInstance()
+      const group = groupInstance.group()
+      let top = null
+      let height = null
+      if (
+        display === GroupDisplayType.Collapsed ||
+        rootInstance.display === GroupDisplayType.Collapsed
+      ) {
+        top = 0
+        height = clipHeight
+      } else {
+        top = clipHeight * groupInstance.row
+        height = group.expandedHeight() * clipHeight
+      }
+      const width = Math.floor(group.length() * pxPerSeconds)
+      const left = groupInstance.start * pxPerSeconds
+      children.push({
+        id: content.id(),
+        kind: GroupContentKindType.Group,
+        props: {
+          id: groupInstance.id,
+          name: group.name,
+          top,
+          left,
+          width,
+          height,
+          color: color,
+          muted: groupInstance.muted,
+          display: groupInstance.display, // always off inside a group
+          displayHeader: false, // always off inside a group
+          children: produceChildrenProps(
+            groupInstance,
+            display,
+            color,
+            pxPerSeconds,
+            clipHeight
+          ),
+        },
+      })
+    }
+  }
+
+  return children
 }
 
 export function groupAdapter({
@@ -31,12 +118,17 @@ export function groupAdapter({
 
   const absoluteStart = parentInstanceStart + groupInstance.start
   const left = (absoluteStart - viewStart) * pxPerSeconds
-
   const width = group.length() * pxPerSeconds
-
   const top = parentTop + clipHeight * groupInstance.row
-
   const height = groupInstance.height() * clipHeight
+
+  const children = produceChildrenProps(
+    groupInstance,
+    groupInstance.display,
+    groupInstance.color,
+    pxPerSeconds,
+    clipHeight
+  )
 
   return {
     id: groupInstance.id,
@@ -48,7 +140,8 @@ export function groupAdapter({
     color: groupInstance.color,
     muted: groupInstance.muted,
     display: groupInstance.display,
-    children: [],
+    displayHeader: true,
+    children,
     onMutedClick,
     onMenuClick,
   }
