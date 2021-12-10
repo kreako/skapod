6e062a08be5484dd7dcb9756dd821915f1af7a9b
kreako <olivier@kreako.fr>
2021-12-10 13:43:41 +0100

    project layout with tests

--- frontend/src/layout/project-layout.test.ts
+++ frontend/src/layout/project-layout.test.ts
@@ -61,34 +61,61 @@ const project = () =>
     },
     clips: {
       "0": { id: "0", name: "", source: "0", start: 11.7, length: 23.8 },
+      "1": { id: "1", name: "", source: "0", start: 31.7, length: 13 },
+      "2": { id: "2", name: "", source: "0", start: 41.8, length: 7 },
     },
     groups: {
       "0": {
         id: "0",
         name: "",
-        content: [],
-      },
-      "1": {
-        id: "1",
-        name: "",
-        content: [],
-      },
-      "2": {
-        id: "2",
-        name: "",
-        content: [],
+        content: [
+          {
+            kind: GroupContentKindType.Clip,
+            data: {
+              id: "g0-0",
+              clip: "1",
+              start: 0,
+              row: 0,
+              muted: false,
+              color: ColorType.Blue,
+            },
+          },
+          {
+            kind: GroupContentKindType.Clip,
+            data: {
+              id: "g0-1",
+              clip: "2",
+              start: 12,
+              row: 1,
+              muted: false,
+              color: ColorType.Blue,
+            },
+          },
+        ],
       },
     },
     content: [
       {
         kind: GroupContentKindType.Clip,
         data: {
-          id: "0",
+          id: "root-0",
           clip: "0",
           start: 3.4,
           row: 0,
           muted: false,
           color: ColorType.Blue,
+        },
+      },
+      {
+        kind: GroupContentKindType.Group,
+        data: {
+          id: "root-1",
+          group: "0",
+          start: 1.2,
+          row: 1,
+          muted: false,
+          color: ColorType.Blue,
+          display: GroupDisplayType.Expanded,
         },
       },
     ],
@@ -104,10 +131,31 @@ test("Layout", () => {
     clipHeight: 100,
     clipMargin: 2,
   }
-  // clip instance "0" is on row 0 and from 3.4s to 27.2s
-  const layout = projectLayout.layoutInfo("0", context)
+  // clip instance "root-0" is on row 0 and from 3.4s to 27.2s
+  let layout = projectLayout.localLayoutStyle("root-0", context)
   expect(layout.top).toBe(0)
   expect(layout.left).toBe((3.4 - 5) * 10)
   expect(layout.width).toBe(23.8 * 10)
+  expect(layout.height).toBe(100)
+
+  // group instance "root-1" is on row 1 and from 1.2s to 20.2s
+  layout = projectLayout.localLayoutStyle("root-1", context)
+  expect(layout.top).toBe(100)
+  expect(layout.left).toBe((1.2 - 5) * 10)
+  expect(layout.width).toBe(19 * 10)
+  expect(layout.height).toBe(200)
+
+  // clip instance "g0-0" is on row 0 and from 0s to 13s
+  layout = projectLayout.localLayoutStyle("g0-0", context)
+  expect(layout.top).toBe(0)
+  expect(layout.left).toBe(0 * 10)
+  expect(layout.width).toBe(13 * 10)
+  expect(layout.height).toBe(100)
+
+  // clip instance "g0-1" is on row 1 and from 12s to 19s
+  layout = projectLayout.localLayoutStyle("g0-1", context)
+  expect(layout.top).toBe(100)
+  expect(layout.left).toBe(12 * 10)
+  expect(layout.width).toBe(7 * 10)
   expect(layout.height).toBe(100)
 })
--- frontend/src/layout/project-layout.ts
+++ frontend/src/layout/project-layout.ts
@@ -1,32 +1,157 @@
+import { ClipInstanceType } from "../api/clip-instance"
+import { GroupType } from "../api/group"
+import { GroupContent } from "../api/group-content"
+import { GroupContentArrayType } from "../api/group-content-array"
+import { GroupInstanceType } from "../api/group-instance"
 import { ProjectType } from "../api/project"
+import { GroupContentKindType, GroupDisplayType } from "../api/types"
 
 export type LayoutContext = {
+  // in seconds (unit)
   viewStart: number
+  // in seconds (unit)
   viewEnd: number
+  // in pixel/seconds (unit)
   pxPerSeconds: number
+  // in pixel (unit)
   clipHeight: number
-  clipMargin: number
 }
 
 export interface ProjectLayoutType {
-  layoutInfo: (id: string, context: LayoutContext) => LayoutInfoType
+  localLayoutStyle: (id: string, context: LayoutContext) => LocalLayoutStyleType
 }
 
-export type LayoutInfoType = {
+export type LocalLayoutInfoType = {
+  // id of the containing group or null for root
+  parentId: string
+  // in row (unit), relative to the group (or root) containing the instance
   top: number
+  // in seconds (unit), relative to the group containing the instance
   left: number
+  // in seconds (unit)
   width: number
+  // in row (unit)
   height: number
+}
+
+export type LocalLayoutStyleType = {
+  // in pixels (unit)
+  top: number
+  // in pixels (unit)
+  left: number
+  // in pixels (unit)
+  width: number
+  // in pixels (unit)
+  height: number
+}
+
+export type LayoutInfoIndexType = {
+  // clipInstance/groupInstance id -> LayoutInfoType
+  [id: string]: LocalLayoutInfoType
+}
+
+function clipLayout(
+  parentId: string,
+  parentDisplay: GroupDisplayType,
+  clipInstance: ClipInstanceType
+): LocalLayoutInfoType {
+  let top = clipInstance.row
+  if (parentDisplay === GroupDisplayType.Collapsed) {
+    top = 0
+  }
+  return {
+    parentId,
+    top,
+    left: clipInstance.start,
+    width: clipInstance.clip().length,
+    height: 1,
+  }
+}
+
+function groupLayout(
+  parentId: string,
+  parentDisplay: GroupDisplayType,
+  groupInstance: GroupInstanceType
+): LocalLayoutInfoType {
+  let top = null
+  let height = null
+  if (
+    parentDisplay === GroupDisplayType.Collapsed ||
+    groupInstance.display === GroupDisplayType.Collapsed
+  ) {
+    top = 0
+    height = 1
+  } else {
+    top = groupInstance.row
+    height = groupInstance.group().expandedHeight()
+  }
+  return {
+    parentId,
+    top,
+    left: groupInstance.start,
+    width: groupInstance.group().length(),
+    height,
+  }
+}
+
+function buildLayoutInfoIndex(
+  parentId: string,
+  display: GroupDisplayType,
+  content: GroupContentArrayType
+): LayoutInfoIndexType {
+  let index = {}
+
+  for (const c of content.content) {
+    if (c.kind === GroupContentKindType.Clip) {
+      const clipInstance = c.asClipInstance()
+      index[clipInstance.id] = clipLayout(parentId, display, clipInstance)
+    } else {
+      const groupInstance = c.asGroupInstance()
+      index[groupInstance.id] = groupLayout(parentId, display, groupInstance)
+      let localDisplay = display
+      if (groupInstance.display === GroupDisplayType.Collapsed) {
+        localDisplay = GroupDisplayType.Collapsed
+      }
+      const group = groupInstance.group()
+      const groupIndex = buildLayoutInfoIndex(
+        groupInstance.id,
+        localDisplay,
+        group.content
+      )
+      index = { ...index, ...groupIndex }
+    }
+  }
+  return index
 }
 
 export class ProjectLayout implements ProjectLayoutType {
   project: ProjectType
+  layout: LayoutInfoIndexType
 
   constructor(project: ProjectType) {
     this.project = project
+    this.layout = buildLayoutInfoIndex(
+      null,
+      GroupDisplayType.Expanded,
+      project.content
+    )
   }
 
-  layoutInfo = (id: string, context: LayoutContext): LayoutInfoType => {
-    return { top: 0, left: 0, width: 100, height: 200 }
+  localLayoutStyle = (
+    id: string,
+    context: LayoutContext
+  ): LocalLayoutStyleType => {
+    // cached info
+    const info = this.layout[id]
+    let left = 0
+    if (info.parentId == null) {
+      left = -context.viewStart * context.pxPerSeconds
+    }
+    return {
+      top: info.top * context.clipHeight,
+      left: left + info.left * context.pxPerSeconds,
+      width: info.width * context.pxPerSeconds,
+      height: info.height * context.clipHeight,
+    }
   }
 }
